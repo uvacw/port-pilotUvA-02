20f84c4312b609d566e4c59d3b181f9bd0ee4643
Theo Araujo <theoaraujo@users.noreply.github.com>
2023-03-15 15:51:24 +0100

    Tiktok logo

--- src/framework/visualisation/react/ui/elements/instructions.tsx
+++ src/framework/visualisation/react/ui/elements/instructions.tsx
@@ -5,6 +5,7 @@ import TwitterSvg from '../../../../../assets/images/twitter.svg'
 import FacebookSvg from '../../../../../assets/images/facebook.svg'
 import InstagramSvg from '../../../../../assets/images/instagram.svg'
 import YoutubeSvg from '../../../../../assets/images/youtube.svg'
+import TiktokSvg from '../../../../../assets/images/tiktok.svg'
 import TextBundle from '../../../../text_bundle'
 import { Bullet } from './bullet'
 
@@ -198,5 +199,5 @@ const icon: Record<string, string> = {
   facebook: FacebookSvg,
   instagram: InstagramSvg,
   youtube: YoutubeSvg,
-  tiktok: YoutubeSvg
+  tiktok: TiktokSvg
 }
